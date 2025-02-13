4bbb60986868cebf4f6b0b43247a8b3098d88ddc
ruffaaw <rafal.p.ciupek@gmail.com>
2025-02-14 00:36:40 +0100

    fix: fixed problem with responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import AboutInvestment from "@/components/AboutInvestment";
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full w-full bg-color3 overflow-hidden">
       <Header />
       <HomeSection />
+      <AboutInvestment />
     </div>
   );
 }
