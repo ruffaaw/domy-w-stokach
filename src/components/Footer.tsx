a0fd670f64c4e086db4a097dd67bbcbe36426a29
ruffaaw <rafalciupek07@gmail.com>
2025-08-21 14:15:43 +0200

    feat: added link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,12 @@ export default function Footer() {
       </section>
 
       <div className="flex flex-col md:flex-row justify-between items-center py-2 px-4 bg-signature text-white">
-        <p className="my-2 text-center md:text-left">2025 Policrafts ©</p>
+        <a
+          href="https://policrafts.com"
+          className="my-2 text-center md:text-left"
+        >
+          2025 Policrafts ©
+        </a>
         <p className="p-0 m-0">
           Realizacja inwestycji:{" "}
           <a
